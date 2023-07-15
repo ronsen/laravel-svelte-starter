4e989bf7906ab7311260441ed93ffe233ebba6db
Ronsen <ronsen@startrek.or.id>
2023-07-15 13:35:06 +0700

    update

--- resources/js/Pages/Posts/Show.svelte
+++ resources/js/Pages/Posts/Show.svelte
@@ -1,7 +1,15 @@
 <script>
+    import { inertia } from "@inertiajs/svelte";
+    import { router } from '@inertiajs/svelte';
     import Layout from "../Layout.svelte";
 
     export let post;
+
+    function destroy(id) {
+        if (confirm('Delete this post?')) {
+            router.delete('/posts/' + id);
+        }
+    }
 </script>
 
 <svelte:head>
@@ -13,8 +21,8 @@
         <div class="flex justify-between items-center border-b border-base-300 pb-3 mb-3">
             <div class="title font-bold">{post.title}</div>
             <div class="inline-flex gap-3">
-                <a href="/posts/{post.id}/edit" title="Edit Post" class="text-gray-500"><i class="bi bi-pencil-square"></i></a>
-                <a href="/posts/{post.id}/delete" title="Delete Post" class="text-gray-500"><i class="bi bi-trash"></i></a>
+                <a use:inertia href="/posts/{post.id}/edit" title="Edit Post" class="text-gray-500"><i class="bi bi-pencil-square"></i></a>
+                <button title="Delete Post" class="text-gray-500" on:click={destroy(post.id)}><i class="bi bi-trash"></i></button>
             </div>
         </div>
     
