02762c9e4f5b5c6e29b46c1d8dd9c1dccd1934c7
Ronsen <ronsen@startrek.or.id>
2023-07-15 17:12:43 +0700

    update

--- resources/js/Pages/Components/Pagination.svelte
+++ resources/js/Pages/Components/Pagination.svelte
@@ -7,15 +7,15 @@
 <div class="text-center mt-6">
     <div class="join gap-3">
         {#if data.prev_page_url}
-            <Link href={data.prev_page_url} class="btn btn-secondary join-item"><i class="bi bi-arrow-left"></i></Link>
+            <Link href={data.prev_page_url} class="join-item"><i class="bi bi-arrow-left"></i></Link>
         {:else}
-            <button class="btn btn-secondary btn-disabled"><i class="bi bi-arrow-left"></i></button>
+            <button class="btn-disabled"><i class="bi bi-arrow-left"></i></button>
         {/if}
 
         {#if data.next_page_url}
-            <Link href={data.next_page_url} class="btn btn-secondary join-item"><i class="bi bi-arrow-right"></i></Link>
+            <Link href={data.next_page_url} class="join-item"><i class="bi bi-arrow-right"></i></Link>
         {:else}
-            <button class="btn btn-secondary btn-disabled"><i class="bi bi-arrow-right"></i></button>
+            <button class="btn-disabled"><i class="bi bi-arrow-right"></i></button>
         {/if}
     </div>
 </div>
