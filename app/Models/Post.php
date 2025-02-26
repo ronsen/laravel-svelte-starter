3ff59b36fc8ccafaa3e621131c57126894e067c4
Ronsen <ronsen@startrek.or.id>
2025-02-26 14:46:04 +0700

    update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-	/** @use HasFactory<\Database\Factories\UserFactory> */
+	/** @use HasFactory<\Database\Factories\PostFactory> */
 	use HasFactory;
 
 	protected $fillable = [
